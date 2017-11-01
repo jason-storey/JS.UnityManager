5830e54bf096eed1cac5cfe47381056173bd0438
Jason Storey <jasonstoreyireland@gmail.com>
2017-11-01 15:33:54 +0000

    Adds Basic Styling

--- JS.UnityManager/MainForm.Designer.cs
+++ JS.UnityManager/MainForm.Designer.cs
@@ -29,11 +29,32 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "notifyIcon";
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseClick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(320, 313);
+            this.Name = "MainForm";
+            this.Text = "Unity Manager";
+            this.Resize += new System.EventHandler(this.MainForm_Resize);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon;
     }
 }
 
--- JS.UnityManager/MainForm.cs
+++ JS.UnityManager/MainForm.cs
@@ -1,12 +1,34 @@
 ﻿using System.Windows.Forms;
+using MetroFramework.Forms;
 
 namespace JS.UnityManager
 {
-    public partial class MainForm : Form
+    public partial class MainForm : MetroForm
     {
         public MainForm()
         {
             InitializeComponent();
+            notifyIcon.Visible = true;
+        }
+
+        private void MainForm_Resize(object sender, System.EventArgs e)
+        {
+            if (FormWindowState.Minimized == this.WindowState)
+            {
+                notifyIcon.Visible = true;
+                this.Hide();
+            }
+            else if (FormWindowState.Normal == this.WindowState)
+            {
+                notifyIcon.Visible = false;
+            }
+        }
+
+        private void notifyIcon_MouseClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+            notifyIcon.Visible = false;
         }
     }
 }
